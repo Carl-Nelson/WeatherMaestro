3e43651de182f6d3a185ebb31b454fc87e2fc7ab
Carl-Nelson <50152324+Carl-Nelson@users.noreply.github.com>
2021-06-01 15:45:00 -0700

    Added Location model

---
+++ app/src/main/java/com/hfad/models/Location.java
@@ -0,0 +1,9 @@
+package com.hfad.models;
+
+public class Location {
+    public String city;
+    public String stateCode;
+    public String zip;
+    public String latitude; //could be a double if needed
+    public String longitude;
+}
